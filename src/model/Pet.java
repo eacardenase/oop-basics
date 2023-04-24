bd14f0a8f77820fd8d8987e2b50cb9922dc2ebb1
Edwin Cardenas <eacardenase@unal.edu.co>
2023-04-23 20:10:57 -0500

    feat: add main class and person cat dog instances to it

--- src/model/Pet.java
+++ src/model/Pet.java
@@ -6,6 +6,8 @@ public class Pet {
     private String name;
     private LocalDate dob;
 
+    public Pet() {}
+
     public Pet(String name, LocalDate dob) {
         this.name = name;
         this.dob = dob;
@@ -25,5 +27,13 @@ public class Pet {
 
     public void setDob(LocalDate dob) {
         this.dob = dob;
+    }
+
+    @Override
+    public String toString() {
+        return "Pet{" +
+                "name='" + name + '\'' +
+                ", dob=" + dob +
+                '}';
     }
 }
