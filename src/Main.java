0a539e619dcb200e613afd322c492fb4703beecb
Edwin Cardenas <eacardenase@unal.edu.co>
2023-04-24 15:14:37 -0500

    feat: working with constructors

--- src/Main.java
+++ src/Main.java
@@ -7,13 +7,7 @@ public class Main {
     public static void main(String[] args) {
 
         Person edwin = new Person("Edwin", "Cardenas", LocalDate.of(1996, 3, 20));
-        Person ana = new Person("Ana", "Torres", LocalDate.of(1998, 2, 15));
-        Dog figo = new Dog("Figo", LocalDate.of(2014, 3, 20));
-
-        edwin.setSpouse(ana);
-        edwin.setPet(figo);
-
-        figo.bark();
+//        Person ana = new Person(); // throws error because of private access level on Person constructor
 
         System.out.println(edwin);
     }
--- src/model/Person.java
+++ src/model/Person.java
@@ -6,9 +6,12 @@ public class Person {
     private String firstname;
     private String lastname;
     private LocalDate dob;
-    private Address address;
-    private Person spouse;
-    private Pet pet;
+
+    // allows creating instances without initializing the values
+    // it's created by default if no other constructor is declared
+    // setting it to private don't allow instantiating without arguments
+    private Person() {
+    }
 
     public Person(String firstname, String lastname, LocalDate dob) {
         this.firstname = firstname;
@@ -38,41 +41,5 @@ public class Person {
 
     public void setDob(LocalDate dob) {
         this.dob = dob;
-    }
-
-    public Address getAddress() {
-        return address;
-    }
-
-    public void setAddress(Address address) {
-        this.address = address;
-    }
-
-    public Person getSpouse() {
-        return spouse;
-    }
-
-    public void setSpouse(Person spouse) {
-        this.spouse = spouse;
-    }
-
-    public Pet getPet() {
-        return pet;
-    }
-
-    public void setPet(Pet pet) {
-        this.pet = pet;
-    }
-
-    @Override
-    public String toString() {
-        return "Person{" +
-                "firstname='" + firstname + '\'' +
-                ", lastname='" + lastname + '\'' +
-                ", dob=" + dob +
-                ", address=" + address +
-                ", spouse=" + spouse +
-                ", pet=" + pet +
-                '}';
     }
 }
